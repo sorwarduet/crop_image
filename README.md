80e5135a85d0195a69660429005fbf2c0818dfcc
Md. Sorwar Alam <esorwar.cse@gmail.com>
2023-12-26 22:03:05 +0600

    Add Image Cropper Project

---
+++ README.md
@@ -0,0 +1,18 @@
+# Simple Image Cropper React JS Project
+
+## What is the use of this Repo
+
+This Project is a Simple ReactJS Image Cropper Project
+
+## Prerequisites
+
+### Install Node JS
+
+Refer to https://nodejs.org/en/ to install nodejs
+
+### Clone the project
+
+```bash
+npm install
+npm run dev
+```
---
+++ src/App.jsx
@@ -0,0 +1,98 @@
+import "./App.css";
+import { Button, Modal } from "keep-react";
+import { Avatar } from "keep-react";
+import Image from "./assets/images/profile.jpg";
+import { useState, createRef } from "react";
+import Cropper from "react-cropper";
+import "cropperjs/dist/cropper.css";
+
+function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  const [image, setImage] = useState("");
+  const [cropData, setCropData] = useState("");
+  const cropperRef = createRef();
+  const handleModal = () => {
+    setShowModal(!showModal);
+  };
+
+  const onChange = (e) => {
+    e.preventDefault();
+    let files;
+    if (e.dataTransfer) {
+      files = e.dataTransfer.files;
+    } else if (e.target) {
+      files = e.target.files;
+    }
+    const reader = new FileReader();
+    reader.onload = () => {
+      setImage(reader.result);
+    };
+    reader.readAsDataURL(files[0]);
+  };
+
+  const getCropData = () => {
+    if (typeof cropperRef.current?.cropper !== "undefined") {
+      setCropData(cropperRef.current?.cropper.getCroppedCanvas().toDataURL());
+    }
+    setShowModal(false);
+  };
+  return (
+    <>
+      <div className="container mx-auto">
+        <div className="mt-5 flex items-center justify-center flex-col">
+          <Avatar shape="circle" size="2xl" img={cropData ? cropData : Image} />
+
+          <Button
+            size="md"
+            className="mt-5"
+            type="dashed"
+            onClick={handleModal}
+          >
+            Upload
+          </Button>
+        </div>
+
+        <Modal size="xl" show={showModal} position="center">
+          <Modal.Body>
+            <div className="flex flex-col items-center">
+              <div className="rounded-full w-[100px] h-[100px] bg-yellow-100 overflow-hidden">
+                <div className="h-full w-full img-preview"></div>
+              </div>
+              <input type="file" className="mt-1 mb-1" onChange={onChange} />
+
+              {image && (
+                <Cropper
+                  ref={cropperRef}
+                  style={{ height: 400, width: "100%" }}
+                  zoomTo={0.5}
+                  initialAspectRatio={1}
+                  preview=".img-preview"
+                  src={image}
+                  viewMode={1}
+                  minCropBoxHeight={10}
+                  minCropBoxWidth={10}
+                  background={false}
+                  responsive={true}
+                  autoCropArea={1}
+                  checkOrientation={false} // https://github.com/fengyuanchen/cropperjs/issues/671
+                  guides={true}
+                />
+              )}
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button type="outlineGray" color="error" onClick={handleModal}>
+              Cancel
+            </Button>
+            <Button type="primary" onClick={getCropData}>
+              Upload
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    </>
+  );
+}
+
+export default App;
